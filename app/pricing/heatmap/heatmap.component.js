68edd3bee7b8687afbb6eafe329874b72191afb0
amohamady <amohamady@kn-it.com>
2016-05-14 15:23:15 +0200

    add zoom in and zoom out

--- app/pricing/heatmap/heatmap.component.js
+++ app/pricing/heatmap/heatmap.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core'], function(exports_1, context_1) {
+System.register(['@angular/core', '../../shared/symbol.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,28 +10,53 @@ System.register(['@angular/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, symbol_service_1;
     var HeatMapComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (symbol_service_1_1) {
+                symbol_service_1 = symbol_service_1_1;
             }],
         execute: function() {
             HeatMapComponent = (function () {
-                function HeatMapComponent() {
+                function HeatMapComponent(symbolService) {
+                    var _this = this;
+                    this.symbolService = symbolService;
+                    this.boxHeight = 20;
+                    this.boxWidth = 20;
                     this.rainbow = new Rainbow();
+                    symbolService.getSymbolsList().then(function (symbolsList) { return _this.symbolsList = symbolsList; });
                 }
                 HeatMapComponent.prototype.ngOnInit = function () {
                     this.rainbow.setSpectrum('#FF0000', '#FFFFFF', '#00FF00');
                     this.rainbow.setNumberRange(0, 100);
                 };
+                HeatMapComponent.prototype.zoomIn = function () {
+                    if (this.boxHeight > 100) {
+                        this.boxHeight = this.boxHeight + 20;
+                    }
+                    if (this.boxWidth > 100) {
+                        this.boxWidth = this.boxWidth + 20;
+                    }
+                };
+                HeatMapComponent.prototype.zoomOut = function () {
+                    if (this.boxHeight > 20) {
+                        this.boxHeight = this.boxHeight - 20;
+                    }
+                    if (this.boxWidth > 20) {
+                        this.boxWidth = this.boxWidth - 20;
+                    }
+                };
                 HeatMapComponent = __decorate([
                     core_1.Component({
                         selector: 'heatmap',
-                        templateUrl: 'heatmap.component.html'
+                        templateUrl: 'heatmap.component.html',
+                        providers: [symbol_service_1.SymbolsService]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [symbol_service_1.SymbolsService])
                 ], HeatMapComponent);
                 return HeatMapComponent;
             }());
--- app/pricing/heatmap/heatmap.component.ts
+++ app/pricing/heatmap/heatmap.component.ts
@@ -1,22 +1,43 @@
 import {Component,OnInit} from '@angular/core';
+import {SymbolsService} from '../../shared/symbol.service';
+import {Symbol} from '../../shared/symbol';
 
 declare var Rainbow: any;
 
 @Component({
   selector: 'heatmap',
-  templateUrl : 'heatmap.component.html'
+  templateUrl : 'heatmap.component.html',
+  providers: [SymbolsService]
 })
 
 
 export class HeatMapComponent implements OnInit {
 rainbow : any;
-boxHeight : string;
-boxWidth
-constructor() {
+boxHeight : number = 20;
+boxWidth : number = 20;
+symbolsList :Symbol[];
+constructor(private symbolService: SymbolsService) {
   this.rainbow = new Rainbow();
+  symbolService.getSymbolsList().then(symbolsList => this.symbolsList = symbolsList);
 }
 ngOnInit() {
   this.rainbow.setSpectrum('#FF0000','#FFFFFF', '#00FF00');
   this.rainbow.setNumberRange(0,100);
+}
+zoomIn(){
+  if(this.boxHeight > 100){
+    this.boxHeight = this.boxHeight+20;
+  }
+  if(this.boxWidth > 100){
+    this.boxWidth = this.boxWidth+20;
+  }
+}
+zoomOut(){
+  if(this.boxHeight > 20){
+    this.boxHeight = this.boxHeight-20;
+  }
+  if(this.boxWidth > 20){
+    this.boxWidth = this.boxWidth-20;
+  }
 }
 }
