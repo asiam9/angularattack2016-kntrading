d074c4eedf3d87745be843760989929500caa2f2
beshir <m.beshirfci@yahoo.com>
2016-05-14 22:41:09 +0200

    place-order form validation

--- app/orders/place-order/place-order.component.ts
+++ app/orders/place-order/place-order.component.ts
@@ -1,29 +1,36 @@
-import {Component} from "@angular/core";
+import {Component,ElementRef,OnInit} from "@angular/core";
 import {Order} from '../shared/order';
 import {SymbolsService} from '../../shared/symbol.service';
 import {Symbol} from '../../shared/symbol';
 import {OrdersService} from '../shared/orders.service';
 
+declare var jQuery:any;
 
 @Component({
   selector: 'place-order',
   templateUrl: 'app/orders/place-order/place-order.component.html',
+  styleUrls:['app/orders/place-order/place-order.component.css'],
   providers:[SymbolsService,OrdersService]
 })
 
-export class PlaceOrderComponent {
-
-
+export class PlaceOrderComponent implements OnInit{
 
   symbolId:string;
   price:number;
   quantity:number;
+  selectedSymbol:Symbol;
+  invalidSymbol:boolean=false;
+  formSubmitted:boolean=false;
 
+  ngOnInit(){
+    jQuery('[data-toggle="tooltip"]').tooltip();
+  }
 
-  constructor(private symbolService:SymbolsService,private ordersService:OrdersService) { }
+  constructor(private symbolService:SymbolsService,private ordersService:OrdersService,private elementRef: ElementRef) { }
 
   getTradeAmount(){
-    return (this.quantity*this.price) | undefined;
+    let tradeAmount=this.quantity*this.price;
+    return (!isNaN(tradeAmount) && tradeAmount>0 ?tradeAmount:'');
   }
   resetValues(){
     this.symbolId=undefined;
@@ -31,11 +38,20 @@ export class PlaceOrderComponent {
     this.quantity=undefined;
   }
 
+  hideModal(){
+    jQuery('#myModal').modal('hide');
+  }
+
   addOrder(isBus:boolean){
-    let symbol:Symbol=this.symbolService.getSymbolById(this.symbolId);
-    if(symbol){
-        this.ordersService.addOrder(new Order(symbol,this.quantity,this.price,isBus,2))
-        this.resetValues();
+    this.formSubmitted=true;
+    this.selectedSymbol=this.symbolService.getSymbolById(this.symbolId);
+    if(this.selectedSymbol){
+      this.invalidSymbol=false;
+      this.ordersService.addOrder(new Order(this.selectedSymbol,this.quantity,this.price,isBus,2))
+      this.resetValues();
+      this.hideModal();
+    }else{
+      this.invalidSymbol=true;
     }
   }
 
