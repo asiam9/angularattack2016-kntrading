dafdb93828c51e5551f4cb1e0a080fe9e09c1bb0
amohamady <amohamady@kn-it.com>
2016-05-14 12:48:51 +0200

    remove shared

--- app/pricing/Shared/market.ts
+++
@@ -1,9 +0,0 @@
-import {MarketSummary} from './pricing/Shared/market-summary';
-import {Symbol} from './pricing/Shared/symbol';
-
-export class Market {
-    id: string;
-    name: string;
-    marketSummary: MarketSummary;
-    symbols: Symbol[];
-}
