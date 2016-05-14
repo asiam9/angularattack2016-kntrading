bad50a247c748a68c16845bd2519c4954d06d8ae
amohamady <amohamady@kn-it.com>
2016-05-14 13:17:43 +0200

    add file

---
+++ app/shared/symbol.ts
@@ -0,0 +1,15 @@
+export class Symbol {
+    id: string;
+    name: string;
+    bidPrice: number;
+    bidSize: number;
+    offerPrice: number;
+    offerSize: number;
+    lastPrice: number;
+    change: number;
+    changeDirection : string;
+    changePercent: number;
+    lastTradeQuantity: number;
+    lastTradeTime: string;
+
+}
