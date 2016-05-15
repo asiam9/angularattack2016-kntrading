163dc0dcb20f8e21f54e0f246e1c28f0b538ea10
Mai-301 <mmsadek@kn-it.com>
2016-05-15 14:35:35 +0200

    commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,12 @@
-import {Component}  from  '@angular/core';
+import {Component,OnInit}  from  '@angular/core';
 import {RouteConfig,ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import {MarketComponent} from './pricing/market/market.component';
 import {HeatMapComponent} from './pricing/heatmap/heatmap.component';
 import {WatchListComponent} from './watchlist/watchlist.component';
 import {PlaceOrderComponent} from './orders/place-order/place-order.component';
 import {OrderMonitorComponent} from './orders/order-monitor/order-monitor.component';
+
+declare var jQuery:any;
 
 @Component({
   selector: 'my-app',
@@ -16,7 +18,8 @@ import {OrderMonitorComponent} from './orders/order-monitor/order-monitor.compon
 @RouteConfig([{
   path:'/market',
   name:'Market',
-  component:MarketComponent
+  component:MarketComponent,
+  useAsDefault:true
 },
 {
   path:'/heatmap',
@@ -34,6 +37,11 @@ import {OrderMonitorComponent} from './orders/order-monitor/order-monitor.compon
   component:OrderMonitorComponent
 }
 ])
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(){
+    jQuery('[data-toggle="tooltip"]').tooltip();
+  }
+
   constructor() { }
 }
