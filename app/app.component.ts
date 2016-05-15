b09b017ff9e67ec7e3e2616b752c14f88ba5349d
beshir <m.beshirfci@yahoo.com>
2016-05-15 12:30:30 +0200

    toltip

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,12 @@
-import {Component}  from  '@angular/core';
+import {Component,OnInit}  from  '@angular/core';
 import {RouteConfig,ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import {MarketComponent} from './pricing/market/market.component';
 import {HeatMapComponent} from './pricing/heatmap/heatmap.component';
 import {WatchListComponent} from './watchlist/watchlist.component';
 import {PlaceOrderComponent} from './orders/place-order/place-order.component';
 import {OrderMonitorComponent} from './orders/order-monitor/order-monitor.component';
+
+declare var jQuery:any;
 
 @Component({
   selector: 'my-app',
@@ -35,6 +37,11 @@ import {OrderMonitorComponent} from './orders/order-monitor/order-monitor.compon
   component:OrderMonitorComponent
 }
 ])
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(){
+    jQuery('[data-toggle="tooltip"]').tooltip();
+  }
+
   constructor() { }
 }
